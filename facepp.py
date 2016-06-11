f678e679bb522de3a9af10540a2a27e861046b42
Julia Guenther <juliag1@alumni.stanford.edu>
2016-06-10 20:56:33 -0700

    Cleaning up

--- facepp.py
+++ facepp.py
@@ -5,11 +5,11 @@
 #
 # This program is free software. It comes without any warranty, to
 # the extent permitted by applicable law. You can redistribute it
-# and/or modify it under the terms of the Do What The Fuck You Want
+# and/or modify it under the terms of the Do What  You Want
 # To Public License, Version 2, as published by Sam Hocevar. See
 # http://sam.zoy.org/wtfpl/COPYING (copied as below) for more details.
 #
-#                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#                DO WHAT  YOU WANT TO PUBLIC LICENSE 
 #                        Version 2, December 2004 
 #
 #     Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
@@ -18,10 +18,10 @@
 #     copies of this license document, and changing it is allowed as long 
 #     as the name is changed. 
 #
-#                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#                DO WHAT  YOU WANT TO PUBLIC LICENSE 
 #       TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
 #
-#      0. You just DO WHAT THE FUCK YOU WANT TO. 
+#      0. You just DO WHAT  YOU WANT TO. 
 
 """a simple facepp sdk
 example:
